e0d30cd1524b28c07e7f2099991d4db428b4cff8
Leon-Kang <coderleonkang@gmail.com>
2023-04-09 23:11:35 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,55 +16,55 @@ import { SITE } from './src/config.mjs';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const whenExternalScripts = (items = []) =>
-  SITE.googleAnalyticsId ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
+    SITE.googleAnalyticsId ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  site: SITE.origin,
-  base: SITE.basePathname,
-  trailingSlash: SITE.trailingSlash ? 'always' : 'never',
+    site: SITE.origin,
+    base: SITE.basePathname,
+    trailingSlash: SITE.trailingSlash ? 'always' : 'never',
 
-  output: 'static',
+    output: 'static',
 
-  markdown: {
-    remarkPlugins: [readingTimeRemarkPlugin],
-  },
-
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    sitemap(),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-    mdx(),
-
-    ...whenExternalScripts(() =>
-      partytown({
-        config: { forward: ['dataLayer.push'] },
-      })
-    ),
-
-    compress({
-      css: true,
-      html: {
-        removeAttributeQuotes: false,
-      },
-      img: false,
-      js: true,
-      svg: false,
-
-      logger: 1,
-    }),
-  ],
-
-  vite: {
-    resolve: {
-      alias: {
-        '~': path.resolve(__dirname, './src'),
-      },
+    markdown: {
+        remarkPlugins: [readingTimeRemarkPlugin],
     },
-  },
+
+    integrations: [
+        tailwind({
+            config: {
+                applyBaseStyles: false,
+            },
+        }),
+        sitemap(),
+        image({
+            serviceEntryPoint: '@astrojs/image/sharp',
+        }),
+        mdx(),
+
+        ...whenExternalScripts(() =>
+            partytown({
+                config: {forward: ['dataLayer.push']},
+            })
+        ),
+
+        compress({
+            css: true,
+            html: {
+                removeAttributeQuotes: false,
+            },
+            img: false,
+            js: true,
+            svg: false,
+
+            logger: 1,
+        }),
+    ],
+
+    vite: {
+        resolve: {
+            alias: {
+                '~': path.resolve(__dirname, './src'),
+            },
+        },
+    },
 });
